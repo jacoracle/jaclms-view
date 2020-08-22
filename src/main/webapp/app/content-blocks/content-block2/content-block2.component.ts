f54cdeece1e3ccbff2776289db6c57a487b2037e
Carlos Madrid <dev.cmadrid@gmail.com>
2020-08-22 18:41:59 -0500

    FEAT: Se finaliza plantilla2 - Texto.

--- src/main/webapp/app/content-blocks/content-block2/content-block2.component.ts
+++ src/main/webapp/app/content-blocks/content-block2/content-block2.component.ts
@@ -1,44 +1,14 @@
-import { Component, Input, Output, EventEmitter, AfterContentInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BloquesCurso } from 'app/shared/model/bloques-curso.model';
-import { ITargetScroll, TargetScroll } from 'app/shared/model/target-scroll.model';
 
 @Component({
   selector: 'jhi-content-block2',
   templateUrl: './content-block2.component.html',
   styleUrls: ['./content-block2.component.scss']
 })
-export class ContentBlock2Component implements AfterContentInit {
+export class ContentBlock2Component {
   @Input() contentBlock?: BloquesCurso;
-  @Output() updateBlock = new EventEmitter();
-  @Input() target?: HTMLElement;
-  @Input() index?: number;
-  newTarget: ITargetScroll[] = [];
 
   constructor() {}
 
-  // Actualizar valor de componente y del bloque de contenido en visorContainer
-  onUpdateComponent($event: Event, index: number): void {
-    this.updateBlock.emit({
-      newValue: $event['newValue'],
-      type: $event['type'],
-      componentIndex: index
-    });
-  }
-
-  ngAfterContentInit(): void {
-    /*
-    this.contentBlocksService.getTarget().subscribe(res => {
-      this.newTarget = res;
-      this.setTarget();
-    });
-    */
-  }
-
-  setTarget(): void {
-    if (this.newTarget !== undefined && this.index !== undefined && this.target != null) {
-      const targetScroll = new TargetScroll(this.index, this.target);
-      this.newTarget.push(targetScroll);
-      // this.contentBlocksService.setTarget(this.newTarget);
-    }
-  }
 }
