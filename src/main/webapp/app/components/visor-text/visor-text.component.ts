acfbb28d0efa623d33ecb0fafb710767478bb96d
Carlos Madrid <dev.cmadrid@gmail.com>
2020-08-22 18:39:02 -0500

    FEAT: Se finaliza componente de texto.

---
+++ src/main/webapp/app/components/visor-text/visor-text.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Componente, IComponente } from 'app/shared/model/componente.model';
+import { Contenido, IContenido } from 'app/shared/model/contenido.model';
+
+@Component({
+  selector: 'jhi-visor-text',
+  templateUrl: './visor-text.component.html',
+  styleUrls: ['./visor-text.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class VisorTextComponent {
+  exampleContent = "<p>Componente sin texto</p>";
+  editing = false;
+  text = '';
+
+  _component?: IComponente;
+  @Input()
+  set component(componente: IComponente | undefined) {
+    this._component = componente;
+  }
+  get component(): IComponente | undefined {
+    return this._component;
+  }   
+
+  constructor() {}
+
+}
