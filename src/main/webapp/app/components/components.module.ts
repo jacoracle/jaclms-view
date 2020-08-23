66ec75a1b62d0f9991fdf49afc03e5a3823080ce
Carlos Madrid <dev.cmadrid@gmail.com>
2020-08-23 17:24:57 -0500

    FEAT: Se finalizó componente imagen.

--- src/main/webapp/app/components/components.module.ts
+++ src/main/webapp/app/components/components.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConstructorImageComponent } from './constructor-image/constructor-image.component';
+import { VisorTextComponent } from './visor-text/visor-text.component';
+import { VisorImageComponent } from './visor-image/visor-image.component';
 import { ConstructorPdfComponent } from './constructor-pdf/constructor-pdf.component';
 import { ConstructorSoundComponent } from './constructor-sound/constructor-sound.component';
-import { VisorTextComponent } from './visor-text/visor-text.component';
 import { ConstructorVideoComponent } from './constructor-video/constructor-video.component';
 
 @NgModule({
   declarations: [
-    ConstructorImageComponent,
+    VisorImageComponent,
     ConstructorPdfComponent,
     ConstructorSoundComponent,
     VisorTextComponent,
     ConstructorVideoComponent
   ],
   exports: [
-    ConstructorImageComponent,
+    VisorImageComponent,
     ConstructorPdfComponent,
     ConstructorSoundComponent,
     VisorTextComponent,
---
+++ src/main/webapp/app/components/visor-image/visor-image.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
+import { IComponente } from 'app/shared/model/componente.model';
+import { FileUploadService } from 'app/services/file-upload.service';
+import { MultimediaService } from 'app/services/multimedia.service';
+
+@Component({
+  selector: 'jhi-visor-image',
+  templateUrl: './visor-image.component.html',
+  styleUrls: ['./visor-image.component.scss']
+})
+export class VisorImageComponent implements OnDestroy {
+  // defaultImageUrl: SafeUrl = './../../../../content/images/img_layout3.png';
+  defaultImageUrl: SafeUrl = '';
+  imgSrc: SafeUrl = '';
+  _component?: IComponente;
+  @Input()
+  set component(componente: IComponente | undefined) {
+    this._component = componente;
+    if(this._component) {
+      this.getImage(this._component);
+    }
+    
+  }
+  get component(): IComponente | undefined {
+    return this._component;
+  }   
+  showLoader = true;
+  subscription?: Subscription;
+
+  constructor(public multimediaService: MultimediaService, private domSanitizer: DomSanitizer) { }
+
+
+  public getImage(component: IComponente): void {
+    if (component && component.contenido && component.contenido.contenido !== '') {
+      this.subscription = this.multimediaService.getImageFile(component.contenido.contenido!).subscribe(data => {
+        this.showLoader = false;
+        const imagePath = URL.createObjectURL(data.body);
+        const objectUrl = this.domSanitizer.bypassSecurityTrustUrl(imagePath);
+        this.imgSrc = objectUrl;
+      });      
+    }
+    else {
+      this.showLoader = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
