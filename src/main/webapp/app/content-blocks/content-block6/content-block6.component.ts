8d3a0b13c60730da23a78670dca4c83d4c766283
Carlos Madrid <dev.cmadrid@gmail.com>
2020-08-23 18:12:06 -0500

    FEAT: Se finalizó plantilla 6 - Texto con video.

--- src/main/webapp/app/content-blocks/content-block6/content-block6.component.ts
+++ src/main/webapp/app/content-blocks/content-block6/content-block6.component.ts
@@ -1,59 +1,14 @@
-import { Component, Input, EventEmitter, Output, AfterContentInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BloquesCurso } from 'app/shared/model/bloques-curso.model';
-import { IComponente } from 'app/shared/model/componente.model';
-import { ITargetScroll, TargetScroll } from 'app/shared/model/target-scroll.model';
 
 @Component({
   selector: 'jhi-content-block6',
   templateUrl: './content-block6.component.html',
   styleUrls: ['./content-block6.component.scss']
 })
-export class ContentBlock6Component implements AfterContentInit {
-  imgSrc = './../../../../content/images/cover_upload.png';
+export class ContentBlock6Component {
   @Input() contentBlock?: BloquesCurso;
-  @Output() updateBlock = new EventEmitter();
-  @Input() target?: HTMLElement;
-  @Input() index?: number;
-  newTarget: ITargetScroll[] = [];
 
   constructor() {}
 
-  // Actualizar valor de componente y del bloque de contenido en visorContainer
-  onUpdateComponent($event: Event, index: number): void {
-    this.updateBlock.emit({
-      newValue: $event['newValue'],
-      type: $event['type'],
-      componentIndex: index
-    });
-  }
-
-  orderTextVideo(components: IComponente[]): IComponente[] {
-    let temp: IComponente[] = [];
-    if (components.length > 1) {
-      if (components[0].tipoComponente!.nombre === 'video') {
-        temp.push(components[1]);
-        temp.push(components[0]);
-      } else {
-        temp = components;
-      }
-    }
-    return temp;
-  }
-
-  ngAfterContentInit(): void {
-    /*
-    this.contentBlocksService.getTarget().subscribe(res => {
-      this.newTarget = res;
-      this.setTarget();
-    });
-    */
-  }
-
-  setTarget(): void {
-    if (this.newTarget !== undefined && this.index !== undefined && this.target != null) {
-      const targetScroll = new TargetScroll(this.index, this.target);
-      this.newTarget.push(targetScroll);
-      // this.contentBlocksService.setTarget(this.newTarget);
-    }
-  }
 }
