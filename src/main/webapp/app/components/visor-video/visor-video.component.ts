1b81776d3d45ae5ba684e0194dfb0c020a20cb64
Carlos Madrid <dev.cmadrid@gmail.com>
2020-08-29 17:27:26 -0500

    FEAT: Se finaliza descarga de videos sobre demanda y pausa de video en curso al reproducir otro.

--- src/main/webapp/app/components/visor-video/visor-video.component.ts
+++ src/main/webapp/app/components/visor-video/visor-video.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnDestroy } from '@angular/core';
-import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
+import { Component, Input, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
 import { IComponente } from 'app/shared/model/componente.model';
 import { MultimediaService } from 'app/services/multimedia.service';
+import { VideoPlayingService } from 'app/services/video-playing.service';
 
 @Component({
   selector: 'jhi-visor-video',
@@ -10,44 +11,76 @@ import { MultimediaService } from 'app/services/multimedia.service';
   styleUrls: ['./visor-video.component.scss']
 })
 export class VisorVideoComponent implements OnDestroy {
-  // defaultVideoUrl: SafeUrl = './../../../../content/images/video.png';
-  defaultVideoUrl: SafeUrl = '';
-  videoSrc: SafeUrl = '';
+
+  defaultSrc: SafeUrl = './../../../../content/images/logo-jhipster.png';
   thumbSrc: SafeUrl = '';
+  videoSrc: SafeUrl = '';
   _component?: IComponente;
   @Input()
   set component(componente: IComponente | undefined) {
     this._component = componente;
-    if(this._component) {
-      this.getVideo(this._component);
-    }
-    
+    if(this._component && this._component.contenido && this._component.contenido.contenido && this._component.contenido.contenido !== "") {
+      this.getThumbnail(this._component.contenido.contenido);
+    } 
   }
   get component(): IComponente | undefined {
     return this._component;
   }  
-  showLoader = true;
+  showLoader = false;
   subscription?: Subscription;
+  @ViewChild('video') video?: ElementRef;
+  active = false;
+  playing = false;
 
-  constructor(private multimediaService: MultimediaService, private domSanitizer: DomSanitizer) {}
+  constructor(private multimediaService: MultimediaService, private videoPlayingService: VideoPlayingService) {
+    this.subscription = this.videoPlayingService.getActive().subscribe(active => {
+      this.active = active;
+    });
+    this.subscription = this.videoPlayingService.getPlaying().subscribe(playing => {
+      if(!this.active) {
+        this.playing = playing;
+        this.pauseVideo();
+      }
+    });
+  }
 
-  public getVideo(component: IComponente): void {
-    if (component && component.contenido && component.contenido.contenido !== '') {
-      this.multimediaService.getVideoThumbnailFile(component.contenido.contenido!).subscribe(data => {
+  private getThumbnail(path: string): void {
+    this.showLoader = true;
+    this.multimediaService.getVideoThumbnailFile(path).subscribe(data => {
+      if(data.body) {
+        this.thumbSrc = this.multimediaService.getSafeUrl(data.body);
         this.showLoader = false;
-        const videoPath = URL.createObjectURL(data.body);
-        const objectUrl = this.domSanitizer.bypassSecurityTrustUrl(videoPath);
-        this.thumbSrc = objectUrl;
+      }      
+    });
+  }
+
+  public loadVideo(): void {
+    this.showLoader = true;
+    if(this.component && this.component.contenido && this.component.contenido.contenido && this.component.contenido.contenido !== "") {
+      this.multimediaService.getVideoFile(this.component.contenido.contenido).subscribe(data=> {
+        if(data.body) {
+          this.videoSrc = this.multimediaService.getSafeUrl(data.body);
+          this.showLoader = false;
+        }
       });
-    }
-    else {
-      this.showLoader = false;
     }
   }
 
   ngOnDestroy(): void {
     if(this.subscription) {
       this.subscription.unsubscribe();
+    }
+  }
+
+  public toggleVideo(): void {
+    this.videoPlayingService.setActive(false);
+    this.active= true;
+    this.videoPlayingService.setPlaying(false);
+  }
+
+  private pauseVideo(): void {
+    if(this.video) {
+      this.video.nativeElement.pause();
     }
   }
 }
--- src/main/webapp/app/services/multimedia.service.ts
+++ src/main/webapp/app/services/multimedia.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { SERVER_API_URL } from 'app/app.constants';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -39,6 +39,49 @@ export class MultimediaService {
       observe: 'response',
       responseType: 'blob'
     });
+  }
+
+  /*
+   * Obtiene video del servidor.
+   * @param filePath  Dirección donde está almacenado el video.
+   * @returns         Video en formato blob.
+   */
+  getVideoFile(filePath: string): Observable<HttpResponse<Blob>> {
+    return this.http.get(SERVER_API_URL + 'api/loadVideo?file=' + filePath, {
+      observe: 'response',
+      responseType: 'blob'
+    });
+  }
+  
+  /*
+   * Solicita thumbnail de video (imagen) del servidor, crea un objectUrl con ella y asigna el src a videoervice.
+   * @param filePath  Dirección donde está almacenada la imagen.
+   */
+  public getVideo(path: string): SafeUrl {
+    let objectUrl: SafeUrl = "";
+    this.getVideoFile(path).subscribe(data => {
+      const videoPath = URL.createObjectURL(data.body);
+      objectUrl = this.domSanitizer.bypassSecurityTrustUrl(videoPath);
+    });
+    return objectUrl;
+  }
+
+  /*
+   * Solicita thumbnail de video (imagen) del servidor, crea un objectUrl con ella y asigna el src a videoervice.
+   * @param filePath  Dirección donde está almacenada la imagen.
+   */
+  public getVideoThumbnail(path: string): SafeUrl {
+    let objectUrl: SafeUrl = "";
+    this.getVideoThumbnailFile(path).subscribe(data => {
+      const videoPath = URL.createObjectURL(data.body);
+      objectUrl = this.domSanitizer.bypassSecurityTrustUrl(videoPath);
+    });
+    return objectUrl;
+  }
+
+  getSafeUrl(file: Blob): SafeUrl {
+    const path = URL.createObjectURL(file);
+    return this.domSanitizer.bypassSecurityTrustUrl(path);
   }
 
 }
