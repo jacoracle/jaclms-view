01804b8d5f91f276e87422663c56fe864d91df84
Carlos Madrid <dev.cmadrid@gmail.com>
2020-11-06 17:16:57 -0600

    FEAT: Se agrega directiva ClickStopPropagation reutilizable. Se utiliza directiva para no seleccionar opción en carga de audios en respuestas de Pregunta.

--- src/main/webapp/app/interactive-activities/interactive-activities.module.ts
+++ src/main/webapp/app/interactive-activities/interactive-activities.module.ts
@@ -4,12 +4,11 @@ import { QuestionComponent } from './question/question.component';
 import { VideoResourceComponent } from './multimedia-components/video-resource/video-resource.component';
 import { AudioResourceComponent } from './multimedia-components/audio-resource/audio-resource.component';
 import { ImageResourceComponent } from './multimedia-components/image-resource/image-resource.component';
+import { ClickStopPropagationModule } from './../directives/click-stop-propagation/click-stop-propagation.module';
 
 @NgModule({
   declarations: [QuestionComponent, VideoResourceComponent, AudioResourceComponent, ImageResourceComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, ClickStopPropagationModule],
   exports: [QuestionComponent]
 })
 export class InteractiveActivitiesModule { }
